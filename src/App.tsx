e022e02e1f904fd5d876219ef1c9d3b6d9bd9993
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-24 17:25:18 +0200

    setup design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="font-sans text-900">Hello world!</h1>
+      <h1 className="font-sans-cond text-800">Hello world!</h1>
+      <h1 className="font-sans text-400">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
