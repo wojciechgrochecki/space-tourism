196878c3c684a8a413c21623e2694e0920c57355
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-27 21:01:36 +0200

    add destination page background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,12 @@ export default {
         "home-mobile": "url('./assets/home/background-home-mobile.jpg')",
         "home-tablet": "url('./assets/home/background-home-tablet.jpg')",
         "home-desktop": "url('./assets/home/background-home-desktop.jpg')",
+        "destination-mobile":
+          "url('./assets/destination/background-destination-mobile.jpg')",
+        "destination-tablet":
+          "url('./assets/destination/background-destination-tablet.jpg')",
+        "destination-desktop":
+          "url('./assets/destination/background-destination-desktop.jpg')",
       },
     },
   },
