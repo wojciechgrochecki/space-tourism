b19879125fc0a300619ed3ac2ad2abd5d58b6de5
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-27 20:44:09 +0200

    add navigation & adjust home content bottom padding

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,16 @@
+import Navigation from "./Navigation";
+
 export default function HomePage({}) {
   return (
     <div
       className="grid min-h-screen grid-rows-[min-content_1fr] overflow-x-hidden bg-dark bg-home-mobile
     bg-cover bg-[bottom_center] bg-no-repeat md:bg-home-tablet lg:bg-home-desktop"
     >
-      <div className="nav h-2">nav here</div>
+      <Navigation />
       <div
         className="grid place-items-center  gap-y-8 px-6 
-      pb-[max(6rem,20vh)] text-center lg:grid-cols-[minmax(2rem,1fr)_repeat(2,minmax(0,450px))_minmax(2rem,1fr)]
-      lg:items-end lg:gap-x-5 xl:gap-x-24 "
+      text-center lg:grid-cols-[minmax(2rem,2fr)_minmax(0,450px)_1fr_minmax(0,450px)_minmax(2rem,2fr)] lg:items-end
+      lg:px-0 lg:pb-[max(6rem,20vh)] "
       >
         <div className="max-w-[450px] lg:col-start-2 ">
           <h1 className="my-4 font-sans-cond text-500 uppercase tracking-widest text-light-blue lg:text-left">
@@ -17,17 +19,17 @@ export default function HomePage({}) {
               Space
             </span>
           </h1>
-          <p className="max-w-[50ch] font-sans text-400 text-light-blue lg:text-left">
+          <p className="max-w-[50ch] font-sans text-400 text-light-blue lg:text-left lg:leading-relaxed">
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
             world experience!
           </p>
         </div>
-        <div className="max-w-[450px] lg:col-start-3 ">
+        <div className="max-w-[450px] lg:col-start-4 ">
           <a
             href="#"
-            className="relative z-10 grid aspect-square place-items-center self-start rounded-full bg-white px-8 font-serif text-[20px]
+            className="relative z-10 grid aspect-square place-items-center rounded-full bg-white px-8 font-serif text-[20px]
             uppercase tracking-[1.25px] text-dark  after:absolute after:-z-10 
             after:block after:h-full after:w-full after:rounded-full after:bg-white/10 after:opacity-0
             after:transition-all after:duration-700 hover:after:scale-150 hover:after:opacity-100 md:text-[2rem]
