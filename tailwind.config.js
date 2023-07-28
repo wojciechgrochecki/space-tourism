f280bbd6b1d15a07a25d1547aa8883fd6ef03962
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-28 20:02:43 +0200

    add TechnologyPage background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,12 @@ export default {
         "crew-mobile": "url('./assets/crew/background-crew-mobile.jpg')",
         "crew-tablet": "url('./assets/crew/background-crew-tablet.jpg')",
         "crew-desktop": "url('./assets/crew/background-crew-desktop.jpg')",
+        "technology-mobile":
+          "url('./assets/technology/background-technology-mobile.jpg')",
+        "technology-tablet":
+          "url('./assets/technology/background-technology-tablet.jpg')",
+        "technology-desktop":
+          "url('./assets/technology/background-technology-desktop.jpg')",
       },
     },
   },
