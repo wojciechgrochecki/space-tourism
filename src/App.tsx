1b5b6234df55c98b8410a4f87d933f1924d6a41a
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-31 22:06:37 +0200

    navigation: create paths to pages with createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,29 @@ import CrewPage from "./components/CrewPage";
 import DestinationPage from "./components/DestinationPage";
 import HomePage from "./components/HomePage";
 import TechnologyPage from "./components/TechnologyPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/crew",
+    element: <CrewPage />,
+  },
+  {
+    path: "/destination",
+    element: <DestinationPage />,
+  },
+  {
+    path: "/technology",
+    element: <TechnologyPage />,
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      {/* <HomePage />
-      <DestinationPage />
-      <CrewPage /> */}
-      <TechnologyPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
