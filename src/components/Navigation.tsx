dd0dd1b206186002c39c04a402c1d4e168e3da12
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-31 22:26:47 +0200

    navigation: add NavLinks to different pages

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,6 +2,7 @@ import Logo from "../assets/shared/Logo.svg";
 import IconClose from "../assets/shared/icon-close.svg";
 import IconHamburger from "../assets/shared/icon-hamburger.svg";
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navigation({}) {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,29 +26,57 @@ export default function Navigation({}) {
           md:static md:h-auto md:w-auto md:translate-x-[0] md:flex-row md:gap-[clamp(1.5rem,5vw,3.5rem)] md:px-[clamp(2rem,10vw,10rem)] md:py-0 md:transition-none 
           ${!isOpen && "translate-x-[100%]"}`}
         >
-          <li className="border-b-[3px] border-b-white/0 font-sans-cond uppercase tracking-wider hover:cursor-pointer hover:border-b-white/40 md:py-8">
-            <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block">
-              00
-            </span>
-            Home
+          <li className=" font-sans-cond uppercase tracking-wider hover:cursor-pointer  ">
+            <NavLink
+              to="/"
+              className={({ isActive }) => `
+                ${isActive && "!border-b-white"}
+                   block h-full border-b-[3px] border-b-white/0 hover:border-b-white/40 md:py-8`}
+            >
+              <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block">
+                00
+              </span>
+              Home
+            </NavLink>
           </li>
-          <li className="border-b-[3px] border-b-white/0 font-sans-cond uppercase tracking-wider hover:cursor-pointer hover:border-b-white/40 md:py-8">
-            <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block">
-              01
-            </span>
-            Destination
+          <li className=" font-sans-cond uppercase tracking-wider hover:cursor-pointer  ">
+            <NavLink
+              to="/destination"
+              className={({ isActive }) => `
+                ${isActive && "!border-b-white"}
+                   block h-full border-b-[3px] border-b-white/0 hover:border-b-white/40 md:py-8`}
+            >
+              <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block">
+                01
+              </span>
+              Destination
+            </NavLink>
           </li>
-          <li className="border-b-[3px] border-b-white/0 font-sans-cond uppercase tracking-wider hover:cursor-pointer hover:border-b-white/40 md:py-8">
-            <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block">
-              02
-            </span>
-            Crew
+          <li className=" font-sans-cond uppercase tracking-wider hover:cursor-pointer  ">
+            <NavLink
+              to="/crew"
+              className={({ isActive }) => `
+                ${isActive && "!border-b-white"}
+                   block h-full border-b-[3px] border-b-white/0 hover:border-b-white/40 md:py-8`}
+            >
+              <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block">
+                02
+              </span>
+              Crew
+            </NavLink>
           </li>
-          <li className="border-b-[3px] border-b-white/0 font-sans-cond uppercase tracking-wider hover:cursor-pointer hover:border-b-white/40 md:py-8">
-            <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block ">
-              03
-            </span>
-            Technology
+          <li className=" font-sans-cond uppercase tracking-wider hover:cursor-pointer  ">
+            <NavLink
+              to="/technology"
+              className={({ isActive }) => `
+                ${isActive && "!border-b-white"}
+                   block h-full border-b-[3px] border-b-white/0 hover:border-b-white/40 md:py-8`}
+            >
+              <span className="mr-[11px] inline-block font-bold md:hidden lg:inline-block">
+                03
+              </span>
+              Technology
+            </NavLink>
           </li>
         </ul>
       </nav>
