a6b05ffcc66074348ead4984af81de023785d668
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-31 23:16:30 +0200

    destination-page: make destination-page content data-driven

--- src/components/DestinationPage.tsx
+++ src/components/DestinationPage.tsx
@@ -1,7 +1,12 @@
 import Navigation from "./Navigation";
 import MoonImg from "../assets/destination/image-moon.png";
+import data from "../../data.json";
+import { useState } from "react";
 
 export default function DestinationPage({}) {
+  const [index, setIndex] = useState(0);
+  const { destinations } = data;
+
   return (
     <div
       className="grid min-h-screen grid-rows-[min-content_1fr] overflow-x-hidden bg-dark bg-destination-mobile
@@ -15,36 +20,39 @@ export default function DestinationPage({}) {
           </span>
           Pick Your Destination
         </h2>
+
         <img
-          src={MoonImg}
-          alt="moon image"
+          src={destinations[index].images.png}
+          alt={"planet " + destinations[index].name + " picture"}
           className="mt-8 max-w-[60%] md:w-[60%] md:max-w-[400px] lg:col-span-1 lg:col-start-2 lg:row-span-2 lg:w-auto  lg:max-w-full lg:self-center"
         />
         <div className="lg:col-span-1 lg:col-start-4 lg:justify-self-start">
           <ul className="mt-6 flex cursor-pointer flex-row gap-6 font-sans-cond text-300 uppercase tracking-wide  md:mt-10 lg:tracking-wider ">
-            <li className="border-b-[3px] border-b-white  pb-2  text-white">
-              Moon
-            </li>
-            <li className="border-b-[3px] border-b-white/40 pb-2 text-light-blue">
-              Mars
-            </li>
-            <li className="border-b-[3px] border-b-white/0 pb-2 text-light-blue">
-              Europa
-            </li>
-            <li className="border-b-[3px] border-b-white/0 pb-2 text-light-blue">
-              Titan
-            </li>
+            {destinations.map((planet, i) => {
+              let style =
+                i === index
+                  ? "border-b-[3px] border-b-white pb-3 text-white"
+                  : "border-b-[3px] border-b-white/0 pb-3 text-light-blue hover:border-b-white/50";
+              return (
+                <li
+                  key={planet.name}
+                  className={style}
+                  onClick={() => {
+                    if (i !== index) setIndex(i);
+                  }}
+                >
+                  {planet.name}
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className="text-center lg:col-span-1 lg:col-start-4 lg:text-left">
           <h1 className="mt-5 font-serif text-800 uppercase text-white md:mt-6">
-            Moon
+            {destinations[index].name}
           </h1>
           <p className="max-w-[62ch] text-center font-sans text-400 leading-6 text-light-blue md:leading-7 lg:text-left lg:leading-8 ">
-            See our planet as you’ve never seen it before. A perfect relaxing
-            trip away to help regain perspective and come back refreshed. While
-            you’re there, take in some history by visiting the Luna 2 and Apollo
-            11 landing sites.
+            {destinations[index].description}
           </p>
           <div className="mt-8 w-full space-y-8 border-t border-t-white/30 py-8  md:mt-14 md:flex md:flex-row md:justify-evenly md:space-y-0 lg:justify-start lg:gap-[min(8vw,10rem)]">
             <div>
@@ -52,7 +60,7 @@ export default function DestinationPage({}) {
                 Avg. distance
               </h3>
               <p className="font-serif text-[1.75rem] uppercase text-white">
-                384,400 km
+                {destinations[index].distance}
               </p>
             </div>
             <div>
@@ -60,7 +68,7 @@ export default function DestinationPage({}) {
                 est. travel time
               </h3>
               <p className="font-serif text-[1.75rem] uppercase text-white">
-                3 days
+                {destinations[index].travel}
               </p>
             </div>
           </div>
